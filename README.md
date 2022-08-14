b38950c99f51bf452ea41ab24206d7d442473e50
root <31116011+MicahBird@users.noreply.github.com>
2022-08-14 15:05:11 -0600

    Add guide and modified program

---
+++ README.md
@@ -0,0 +1,49 @@
+# ESP32 Countdown LED Matrix
+---
+### Credit goes to [rznazn](https://github.com/rznazn/Christmas_count_down) for the orginal program.
+
+![image](image.jpg)
+
+## Setting Wi-Fi
+To set the Wi-Fi, change the following lines in `esp32-countdown.ino`.
+
+```
+// Set Wi-Fi name and Password here
+const char* ssid       = "My SSID";
+const char* password   = "My Password";
+```
+
+## Setting Timezone
+The upstream timezone is UTC, to change the following line to offset the time for your timezone. For example, this would be the offset for Central time.
+
+```
+// Timezone offset
+const long  gmtOffset_sec = 3600 * -6;
+```
+
+## Setting Countdown Day
+The default day to countdown to is Christmas Day, to adjust the day, subtrack the the number of days between the day that you want to countdown to, and chrismas day. Then adjust the following line replacing zero with the desired number.
+
+```
+  // Change Day
+  daysTill = (daysTill - 0);
+```
+
+## Turn Display off During Certain Times
+The current time is stored in the 24-Hour Format, and to disable the display, just create an if statement after line 137. For example, this would turn off the display at midnight.
+```
+    if (timeinfo.tm_hour == 24) {
+     display.clearDisplay();
+    }
+```
+
+## Change Display Brightness
+To change the display brightness, change any of the of the following lines to the desired value.
+
+`display.setBrightness(200); // Set Display Brightness Default is 255`
+
+## Limitations
+It is untested if leap years are calculated properly.
+
+## Compatible boards
+Tested on the DOIT ESP32 DEVKIT V1
--- esp32-countdown.ino
+++ esp32-countdown.ino
@@ -25,11 +25,12 @@ uint16_t myCYAN = display.color565(0, 255, 255);
 uint16_t myMAGENTA = display.color565(255, 0, 255);
 uint16_t myBLACK = display.color565(0, 0, 0);
 
-
-const char* ssid       = "my ssid";
-const char* password   = "my password";
+// Set Wi-Fi name and Password here
+const char* ssid       = "My SSID";
+const char* password   = "My Password";
 
 const char* ntpServer = "pool.ntp.org";
+// Timezone offset
 const long  gmtOffset_sec = 3600 * -6;
 const int   daylightOffset_sec = 3600;
 
@@ -77,18 +78,21 @@ void setup()
   display.clearDisplay();
   display.setTextColor(myWHITE);
   display.setCursor(5,7);
-  display.print("HackerBox");
+  display.print("Booting..");
 }
 
 void loop()
 {
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   struct tm timeinfo;
+  // If Wi-Fi Fails
   if(!getLocalTime(&timeinfo)){
     display.clearDisplay();
     display.setTextColor(myWHITE);
     display.setCursor(5,7);
-    display.print("fail");
+    display.print("Failed");
+    display.setCursor(5,18);
+    display.print("Wi-Fi?");
     return;
   }  
   display.clearDisplay();
@@ -100,24 +104,61 @@ void loop()
   if (timeinfo.tm_year % 4 == 0 && timeinfo.tm_mon > 1) daysOfMonthsPast++;
   int daysTill = timeinfo.tm_year % 4 == 0 ? 360 -(timeinfo.tm_mday + daysOfMonthsPast)  : 359 - (timeinfo.tm_mday + daysOfMonthsPast) ;
   if (daysTill < 0) daysTill =  (timeinfo.tm_year + 1)% 4 == 0 ? 366 + daysTill : 365 + daysTill;
+  
+  // Change Day
+  daysTill = (daysTill - 0);
+  
   String timeStr = String(daysTill) + " Days";
   if (daysTill == 0)
   {
+      display.drawLine(0,0,63,0,myYELLOW);
+      display.drawLine(0,31,63,31,myYELLOW);
+      display.drawLine(0,1,0,30,myBLUE);
+      display.drawLine(63,1,63,30,myBLUE);
       display.setCursor(5,7); 
       display.setTextColor(myGREEN);
-      display.print("Merry");
+      display.print("Now this");
       display.setCursor(5,18);
       display.setTextColor(myRED);
-      display.print("Christmas");
+      display.print("is Epic");
   }
   else
   {
+       display.setBrightness(200); // Set Display Brightness Default is 255
+
+    digitalWrite(LED_BUILTIN, LOW);
+    display.setBrightness(200); // Set Display Brightness Default is 255
     display.setTextColor(myWHITE);
     display.setCursor(5,7); 
     display.print(timeStr);
-    display.setCursor(5,18); 
-    display.print("Till Xmas");
+    display.setCursor(5,18);
+    display.print("Until Day");
+    
+    // If statements to turn off display during night 
+    if (timeinfo.tm_hour == 22) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 23) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 24) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 1) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 2) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 3) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 4) {
+     display.clearDisplay();
+    }
+    if (timeinfo.tm_hour == 5) {
+     display.clearDisplay();
+    }
   }
- 
   delay(1000 * 60 * 60);
 }
